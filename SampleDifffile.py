a06a8a547f44a269e858929c3b0f3fe58bac618f
VSVenkat <venkat.vallam@gmail.com>
2024-01-04 18:24:17 +0530

    Create SampleDifffile.py

---
+++ SampleDifffile.py
@@ -0,0 +1,5 @@
+def get_different_rows(source_df, new_df):
+    """Returns just the rows from the new dataframe that differ from the source dataframe"""
+    merged_df = source_df.merge(new_df, indicator=True, how='outer')
+    changed_rows_df = merged_df[merged_df['_merge'] == 'right_only']
+    return changed_rows_df.drop('_merge', axis=1)
